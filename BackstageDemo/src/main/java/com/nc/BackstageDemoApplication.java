4555a3516d54338543a11d5294567dea5c3239b0
NC9596 <nandi9596@gmail.com>
2023-05-05 12:19:01 +0530

    DemoProjPush

---
+++ BackstageDemo/src/main/java/com/nc/BackstageDemoApplication.java
@@ -0,0 +1,14 @@
+package com.nc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackstageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackstageDemoApplication.class, args);
+		System.out.println("Welcome to Backstage Demo!!!");
+	}
+
+}
